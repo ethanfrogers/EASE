d110aad11d756c8d3e8e08ba6367a31f9cb22e07
Ethan Rogers <ethanfrogers@gmail.com>
2013-01-31 13:28:11 -0500

    Create an abstract ContolPanel

---
+++ EASE/src/MVCFramework/AbstractControlPanel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVCFramework;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ *
+ * @author efrogers_it
+ */
+public class AbstractControlPanel extends JPanel{
+    
+    private DisplayPanel view;
+    private Ensemble model;
+    
+    
+}
