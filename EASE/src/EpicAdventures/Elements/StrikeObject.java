382adbe9e4fc8aefd1dc5d3e99eaf8f2399e962a
Ethan <Ethan@ethan>
2013-02-10 10:38:31 -0500

    a strike Objects class for objects that can strike a target

---
+++ EASE/src/EpicAdventures/Elements/StrikeObject.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EpicAdventures.Elements;
+
+/**
+ *
+ * @author Ethan
+ */
+public abstract class StrikeObject extends AbstractGameObject {
+    
+    
+}
